e6deae5283d753f8d45a01e482dc533db28a488b
Lucas Rios <lucasrius@gmail.com>
2021-10-07 21:54:21 -0300

    Selenium dockerizado

--- src/test/java/br/ce/lrsantos/tasks/functional/FunctionalTest.java
+++ src/test/java/br/ce/lrsantos/tasks/functional/FunctionalTest.java
@@ -1,25 +1,31 @@
 package br.ce.lrsantos.tasks.functional;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class FunctionalTest {
 	
 	
-	public WebDriver acessarAplicacao() {
-		WebDriver driver = new ChromeDriver();
-		driver.navigate().to("http://localhost:8001/tasks");
+	public WebDriver acessarAplicacao() throws MalformedURLException {
+		//WebDriver driver = new ChromeDriver();
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.15:4444/wd/hub"), cap);
+		driver.navigate().to("http://192.168.0.15:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
 
 	@Test
-	public void deveSalvarTarefa() {
+	public void deveSalvarTarefa() throws MalformedURLException {
 		WebDriver driver = acessarAplicacao();
 		try {
 		//Clicar em add todo
@@ -42,7 +48,7 @@ public class FunctionalTest {
 	}
 	
 	@Test
-	public void naoDeveSalvarTarefaSemDescricao() {
+	public void naoDeveSalvarTarefaSemDescricao() throws MalformedURLException {
 		WebDriver driver = acessarAplicacao();
 		try {
 		//Clicar em add todo
@@ -65,7 +71,7 @@ public class FunctionalTest {
 	}
 	
 	@Test
-	public void naoDeveSalvarTarefaSemData() {
+	public void naoDeveSalvarTarefaSemData() throws MalformedURLException {
 		WebDriver driver = acessarAplicacao();
 		try {
 		//Clicar em add todo
@@ -88,7 +94,7 @@ public class FunctionalTest {
 	}
 	
 	@Test
-	public void naoDeveSalvarTarefaComDataPassada() {
+	public void naoDeveSalvarTarefaComDataPassada() throws MalformedURLException {
 		WebDriver driver = acessarAplicacao();
 		try {
 		//Clicar em add todo
